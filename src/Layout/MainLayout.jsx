16c954d5bc606e03639f668b72fc476ecae014e6
developersajeeb <sajeebdeveloper@gmail.com>
2023-06-07 17:09:47 +0600

    install react icon and humbergur manu

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import NavBar from "../shared/NavBar";
 
 
 const MainLayout = () => {
     return (
         <>
+            <NavBar></NavBar>
             <Outlet></Outlet>
         </>
     );
